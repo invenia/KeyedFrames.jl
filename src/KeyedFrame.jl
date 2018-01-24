8cb5e317567a8063b3cc5cb69b9858472261b486
Gem Newman <spurll@gmail.com>
2018-01-23 18:27:32 -0600

    First real commit; needs tests

--- src/KeyedFrame.jl
+++ src/KeyedFrame.jl
@@ -1,6 +1,94 @@
 __precompile__()
 module KeyedFrame
 
-# Package code goes here.
+using DataFrames
+import DataFrames: SubDataFrame, OnType, nrow, ncol, index, getindex, setindex!, sort!,
+       push!, append!, deleterows!, head, tail
+
+struct KeyedFrame <: AbstractDataFrame
+    frame::DataFrame
+    key::Vector{<:OnType}
+
+    function KeyedFrame(df::DataFrame, key::Vector{<:OnType})
+        if !issubset(key, names(df))
+            throw(
+                ArgumentError(
+                    "The index columns provided must all be present in the DataFrame."
+                )
+            )
+        end
+
+        return new(df, key)
+    end
+end
+
+function KeyedFrame(df::DataFrame, key::Vector{<:AbstractString})
+    return KeyedFrame(df, map(Symbol, key))
+end
+
+KeyedFrame(df::DataFrame, key::OnType) = KeyedFrame(df, [key])
+
+"""
+    KeyedFrame(df::DataFrame, key::Vector)
+
+Create an `KeyedFrame` using the provided `DataFrame`; `key` specifies the columns
+to use by default when performing a `join` on `KeyedFrame`s when `on` is not provided.
+
+When performing a `join`, if only one of the arguments is an `KeyedFrame` and `on` is not
+specified, the frames will be joined on the `key` of the `KeyedFrame`. If both
+arguments are `KeyedFrame`s, `on` will default to the intersection of their respective
+indices. In all cases, the result of the `join` will share a type with the first argument.
+"""
+KeyedFrame
+
+Base.convert(::Type{DataFrame}, frame::KeyedFrame) = frame.df
+
+nrow(kf::KeyedFrame) = nrow(kf.frame)
+ncol(kf::KeyedFrame) = ncol(kf.frame)
+
+index(kf::KeyedFrame) = index(kf.frame)
+getindex(kf::KeyedFrame, indices...) = getindex(kf.frame, indices...)
+setindex!(kf::KeyedFrame, value, ind...) = setindex!(kf.frame, value, ind...)
+key(kf::KeyedFrame) = kf.key
+
+sort!(kf::KeyedFrame; kwargs...) = sort!(kf.frame; kwargs...)
+SubDataFrame(kf::KeyedFrame, args...) = SubDataFrame(kf.frame, args...)
+
+push!(kf::KeyedFrame, data) = push!(kf.frame, data)
+append!(kf::KeyedFrame, data) = append!(kf.frame, data)
+deleterows!(kf::KeyedFrame, ind) = deleterows!(kf.frame, ind)
+
+# Returns an KeyedFrame
+function Base.join(a::KeyedFrame, b::KeyedFrame; on=nothing, kwargs...)
+    key = intersect(a.key, b.key)
+    return KeyedFrame(join(a.frame, b.frame; on=on === nothing ? key : on, kwargs...), key)
+end
+
+# Returns an KeyedFrame
+function Base.join(a::KeyedFrame, b::AbstractDataFrame; on=nothing, kwargs...)
+    return KeyedFrame(
+        join(a.frame, b.frame; on=on === nothing ? a.key : on, kwargs...), a.key
+    )
+end
+
+# Does NOT return an KeyedFrame
+function Base.join(a::AbstractDataFrame, b::KeyedFrame; on=nothing, kwargs...)
+    return join(a.frame, b.frame; on=on === nothing ? b.key : on, kwargs...)
+end
+
+head(kf::KeyedFrame, args...) = KeyedFrame(head(kf.frame, args...), kf.key)
+tail(kf::KeyedFrame, args...) = KeyedFrame(tail(kf.frame, args...), kf.key)
+
+function Base.permute!(df::DataFrame, index::AbstractVector)
+    permute!(df.columns, index)
+    df.colindex = DataFrames.Index(
+        Dict(df.colindex.names[j] => i for (i, j) in enumerate(index)),
+        [df.colindex.names[j] for j in index]
+    )
+end
+
+Base.permute!(kf::KeyedFrame, index::AbstractVector) = permute!(kf.frame, index)
+
+export KeyedFrame, key
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,49 @@
 using KeyedFrame
 using Base.Test
 
-@testset "KeyedFrame.jl" begin
-    # Write your own tests here.
-    @test 1 == 2
+@testset "KeyedFrame" begin
+    df1 = DataFrame(a=1:10, b=2:11, c=3:12)
+    df2 = DataFrame(a=1:5, d=4:8)
+    df3 = DataFrame(a=1:5, b=2:6, e=5:9)
+
+    @testset "constructor" begin
+        @test false
+    end
+
+    @testset "convert" begin
+        @test false
+    end
+
+    @testset "size" begin
+        @test false
+    end
+
+    @testset "index" begin
+        @test false
+    end
+
+    @testset "size" begin
+        @test false
+    end
+
+    @testset "sort" begin
+        @test false
+    end
+
+    @testset "modify" begin
+        @test false
+    end
+
+    @testset "join" begin
+        # TODO Test join with both index frames and half index frames
+        @test false
+    end
+
+    @testset "head/tail" begin
+        @test false
+    end
+
+    @testset "permute" begin
+        @test false
+    end
 end
